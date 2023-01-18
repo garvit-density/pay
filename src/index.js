daf0d3855681e3173a0fa8869990c6a75105ff0a
garvitvirmani <garvitvirmani@gmail.com>
2023-01-18 13:12:03 +0530

    modal-started

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
 import Pay from "./Pay";
 import Withdraw from "./Withdraw";
+import Modal from "./components/Modal";
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      {/* <Pay /> */}
-      <Withdraw />
+      <Pay />
+      {/* <Withdraw /> */}
+      <Modal></Modal>
     </StyledEngineProvider>
   </React.StrictMode>
 );
